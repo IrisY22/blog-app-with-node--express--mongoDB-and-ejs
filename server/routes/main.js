caf3fe9e354700e3ea6d5caffaca5e54850bdddb
Iris Yatskaer <iriska1160@gmail.com>
2024-02-04 19:09:48 +0200

    Add page to open blog when clicking

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,30 +3,92 @@ const router = express.Router();
 const Post = require('../models/Post')
 
 // Routes
-router.get("", async (req, res) => {
-  const locals = {
-    title: "NodeJs Blog",
-    description: "Simple blog created with NodeJs, Express & MongoDB.",
-  };
 
+// GET Home
+
+router.get("", async (req, res) => {
   try {
-    const data = await Post.find()
-    res.render("index", { locals, data });
+    const locals = {
+      title: "NodeJs Blog",
+      description: "Simple blog created with NodeJs, Express & MongoDB.",
+    };
+
+    let perPage = 6;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await Post.countDocuments();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+
+    res.render('index', {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null
+    });
   } catch (error) {
     console.log(error);
   }
 
 });
 
-function insertPostData() {
-  Post.insertMany([
-    {
-      title: 'Building a Blog',
-      body: 'This is the body text'
-    },
-  ])
-}
-insertPostData();
+// GET Post :id
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    const locals = {
+      title: "NodeJs Blog",
+      description: "Simple blog created with NodeJs, Express & MongoDB.",
+    };
+
+    let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug })
+    res.render('post', { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+
+
+
+
+
+
+
+// router.get("", async (req, res) => {
+//   const locals = {
+//     title: "NodeJs Blog",
+//     description: "Simple blog created with NodeJs, Express & MongoDB.",
+//   };
+
+//   try {
+//     const data = await Post.find()
+//     res.render("index", { locals, data });
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+// });
+
+// function insertPostData() {
+//   Post.insertMany([
+//     {
+//       title: 'Building a Blog',
+//       body: 'This is the body text'
+//     },
+//   ])
+// }
+// insertPostData();
 
 
 
