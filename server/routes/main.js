2f2f38f6bf25414db9214790360aa3ac592a3a86
Iris Yatskaer <iriska1160@gmail.com>
2024-02-04 16:44:06 +0200

    Add AtlasDB

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const Post = require('../models/Post')
 
 // Routes
-router.get("", (req, res) => {
+router.get("", async (req, res) => {
   const locals = {
     title: "NodeJs Blog",
     description: "Simple blog created with NodeJs, Express & MongoDB.",
   };
 
-  res.render("index", { locals });
+  try {
+    const data = await Post.find()
+    res.render("index", { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
+
 });
+
+function insertPostData() {
+  Post.insertMany([
+    {
+      title: 'Building a Blog',
+      body: 'This is the body text'
+    },
+  ])
+}
+insertPostData();
+
+
+
 
 router.get("/about", (req, res) => {
   res.render("about");
