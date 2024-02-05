c3caa0428516ca68204e137ecb61a39a1ae46f48
Iris Yatskaer <iriska1160@gmail.com>
2024-02-05 16:18:42 +0200

    Add cookie parser and dashboard

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const Post = require('../models/Post');
-const User = require('../models/User')
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 const adminLayout = '../views/layouts/admin'
+const jwtSecret = process.env.JWT_SECRET
 
 // Routes
+
+// Check Login
+const authMiddleWare = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unautharized' })
+  }
+
+  try {
+    const decoded = jwt.vwrify(token, jwtSecret);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Unautharized' })
+  }
+}
+
+
 
 // GET Admin - Login Page
 
@@ -32,11 +54,56 @@ router.post("/admin", async (req, res) => {
 
     const { username, password } = req.body;
 
-    if (req.body.username === 'admin' && req.body.password === 'password') {
-      res.send('You are logged in.')
-    } else res.send('Wrong username or password')
-    res.redirect('/admin')
-    res.render("admin/index", { locals, layout: adminLayout });
+    const user = await User.findOne({ username })
+
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid credentials' })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid credentials' })
+    }
+
+    const token = jwt.sign({ userId: user._id }, jwtSecret);
+    res.cookie('token', token, { httpOnly: true });
+
+    res.redirect('/dashboard')
+
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+// POST Admin - Check Login
+
+
+router.get("/dashboard", async (req, res) => {
+
+  res.render('admin/dashboard');
+});
+
+// POST Admin - Register
+
+
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    try {
+      const user = await User.create({ username, password: hashedPassword });
+      res.status(201).json({ message: 'User created', user })
+    } catch (error) {
+      if (error.code === 11000) {
+        res.status(409).json({ message: 'User already in use' });
+      }
+      res.status(500).json({ message: 'Internal server error' })
+    }
+
+
   } catch (error) {
     console.log(error);
   }
